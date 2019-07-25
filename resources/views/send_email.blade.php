c5c5a3ef67e981964d3c24c42eb809daa20cf677
= <=>
2019-07-25 14:01:58 +0100

    initial commit

---
+++ resources/views/send_email.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>How Send an Email in Laravel</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <style type="text/css">
+        .box {
+            width: 600px;
+            margin: 0 auto;
+            border: 1px solid #ccc;
+        }
+
+        .has-error {
+            border-color: #cc0000;
+            background-color: #ffff99;
+        }
+    </style>
+</head>
+
+<body>
+    <br />
+    <br />
+    <br />
+    <div class="container box">
+        <h3 align="center">How Send an Email in Laravel</h3><br />
+        @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <button type="button" class="close" data-dismiss="alert">×</button>
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+        @if ($message = Session::get('success'))
+        <div class="alert alert-success alert-block">
+            <button type="button" class="close" data-dismiss="alert">×</button>
+            <strong>{{ $message }}</strong>
+        </div>
+        @endif
+        <form method="post" action="{{url('sendemail/send')}}">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label>Enter Your Name</label>
+                <input type="text" name="name" class="form-control" value="" />
+            </div>
+            <div class="form-group">
+                <label>Enter Your Email</label>
+                <input type="text" name="email" class="form-control" value="" />
+            </div>
+            <div class="form-group">
+                <label>Enter Your Message</label>
+                <textarea name="message" class="form-control"></textarea>
+            </div>
+            <div class="form-group">
+                <input type="submit" name="send" class="btn btn-info" value="Send" />
+            </div>
+        </form>
+
+    </div>
+</body>
+
+</html>
